388b3a05e8f902cc069a41e7ee83a7de939a31df
kroko <sopilyugin@gmail.com>
2023-02-18 22:47:11 +0300

    Step 4.6 update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,4 +1,5 @@
 import * as fs from 'node:fs';
+import path from 'node:path';
 import genDiff from '../src/gendiff.js';
 
 let filePath1;
@@ -9,22 +10,22 @@ let filePathDiff2;
 let filePathDiff3;
 
 beforeAll(() => {
-  filePath1 = '/mnt/c/Code/frontend-project-46/__tests__/__fixtures__/file1.json';
-  filePath2 = '/mnt/c/Code/frontend-project-46/__tests__/__fixtures__/file2.json';
-  filePathEmpty = '/mnt/c/Code/frontend-project-46/__tests__/__fixtures__/empty.json';
-  filePathDiff1 = '/mnt/c/Code/frontend-project-46/__tests__/__fixtures__/diff1.txt';
-  filePathDiff2 = '/mnt/c/Code/frontend-project-46/__tests__/__fixtures__/diff2.txt';
-  filePathDiff3 = '/mnt/c/Code/frontend-project-46/__tests__/__fixtures__/diff3.txt';
+  filePath1 = '__tests__/__fixtures__/file1.json';
+  filePath2 = '__tests__/__fixtures__/file2.json';
+  filePathEmpty = '__tests__/__fixtures__/empty.json';
+  filePathDiff1 = '__tests__/__fixtures__/diff1.txt';
+  filePathDiff2 = '__tests__/__fixtures__/diff2.txt';
+  filePathDiff3 = '__tests__/__fixtures__/diff3.txt';
 });
 
 test('diff test', () => {
-  const diff1 = fs.readFileSync(filePathDiff1, 'utf8');
+  const diff1 = fs.readFileSync(path.resolve(filePathDiff1), 'utf8');
   expect(genDiff(filePath1, filePath2)).toBe(diff1);
 
-  const diff2 = fs.readFileSync(filePathDiff2, 'utf8');
+  const diff2 = fs.readFileSync(path.resolve(filePathDiff2), 'utf8');
   expect(genDiff(filePath1, filePath1)).toBe(diff2);
 
-  const diff3 = fs.readFileSync(filePathDiff3, 'utf8');
+  const diff3 = fs.readFileSync(path.resolve(filePathDiff3), 'utf8');
   expect(genDiff(filePath1, filePathEmpty)).toBe(diff3);
 
   const diff4 = '{\n  \n}';
